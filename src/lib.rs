a7280f2918f0fa775e769075df8120be836fba1c
Jax Young <jaxvanyang@gmail.com>
2026-05-14 17:54:27 +0800

    refactor!: rename module vector -> vecmath
    This rename matches the PBRT's naming.

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,11 @@ pub mod core;
 pub mod matrix;
 pub mod screen;
 pub mod shapes;
-pub mod vector;
+pub mod vecmath;
 
 pub use camera::*;
 pub use core::*;
 pub use matrix::*;
 pub use screen::*;
 pub use shapes::*;
-pub use vector::*;
+pub use vecmath::*;
